399aa414471dc5f742e1c4d5903ac6a59f21dfea
codemountains <4kz12zz@gmail.com>
2022-11-23 10:39:03 +0900

    :sparkles: getGroups

---
+++ src/client/group/groups.rs
@@ -0,0 +1,78 @@
+use crate::client::query::query_string;
+use crate::client::HEADER_KEY;
+use crate::response::error::{ErrorResponse, ErrorResponseBody};
+use crate::response::group::groups::{GetGroupsResponse, GetGroupsResponseBody};
+use std::collections::HashMap;
+
+const RESOURCE: &str = "groups";
+
+pub struct Groups {
+    url: String,
+    authorization_header: String,
+}
+
+impl Groups {
+    pub fn new(url: &str, authorization_header: &str) -> Self {
+        Self {
+            url: url.to_string() + RESOURCE,
+            authorization_header: authorization_header.to_string(),
+        }
+    }
+
+    pub async fn get(
+        &self,
+        query_params: HashMap<String, String>,
+    ) -> Result<GetGroupsResponse, ErrorResponse> {
+        let request_url = format!("{}?{}", &self.url, query_string(query_params));
+
+        let http_client = reqwest::Client::new();
+        let result = http_client
+            .get(request_url)
+            .header(HEADER_KEY, &self.authorization_header)
+            .send()
+            .await;
+
+        match result {
+            Ok(resp) => {
+                let status = resp.status().as_u16();
+
+                if status == 200 {
+                    match resp.json::<GetGroupsResponseBody>().await {
+                        Ok(body) => Ok(GetGroupsResponse { status, body }),
+                        Err(err) => {
+                            let body = ErrorResponseBody {
+                                error: true,
+                                messages: vec![err.to_string()],
+                            };
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                    }
+                } else {
+                    match resp.json::<ErrorResponseBody>().await {
+                        Ok(body) => {
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                        Err(err) => {
+                            let body = ErrorResponseBody {
+                                error: true,
+                                messages: vec![err.to_string()],
+                            };
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                    }
+                }
+            }
+            Err(err) => {
+                let body = ErrorResponseBody {
+                    error: true,
+                    messages: vec![err.to_string()],
+                };
+                let error_response = ErrorResponse { status: 500, body };
+                Err(error_response)
+            }
+        }
+    }
+}
