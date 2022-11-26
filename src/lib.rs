3cd3018c2040df690378240f9337d5d5be4535e6
codemountains <4kz12zz@gmail.com>
2022-11-26 20:36:54 +0900

    :sparkles: use query builder

--- src/lib.rs
+++ src/lib.rs
@@ -278,16 +278,14 @@ mod tests {
 
     #[tokio::test]
     async fn users_works() {
-        let query_params = HashMap::new();
-
+        let query = Query::default();
         let client = client_new_by_api_key();
-        let resp = client.users.get(query_params).await;
+        let resp = client.users.get(query).await;
         assert_eq!(true, resp.is_ok());
 
-        let query_params = HashMap::new();
-
+        let query = Query::default();
         let client = client_new_by_password();
-        let resp = client.users.get(query_params).await;
+        let resp = client.users.get(query).await;
         assert_eq!(true, resp.is_ok());
     }
 
