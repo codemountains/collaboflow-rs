c2bdb4b5b99cc7048a1edd51b1ae48c22fed1ff6
codemountains <4kz12zz@gmail.com>
2022-11-26 20:25:01 +0900

    :sparkles: use query builder

--- src/lib.rs
+++ src/lib.rs
@@ -210,18 +210,16 @@ mod tests {
 
     #[tokio::test]
     async fn myprocesses_works() {
-        let mut query_params = HashMap::new();
-        query_params.insert("app_cd".to_string(), app_cd());
+        let app_cd = 1;
 
+        let query = Query::builder().app_cd(app_cd);
         let client = client_new_by_api_key();
-        let resp = client.myprocesses.get(query_params).await;
+        let resp = client.myprocesses.get(query).await;
         assert_eq!(true, resp.is_ok());
 
-        let mut query_params = HashMap::new();
-        query_params.insert("app_cd".to_string(), app_cd());
-
+        let query = Query::builder().app_cd(app_cd);
         let client = client_new_by_password();
-        let resp = client.myprocesses.get(query_params).await;
+        let resp = client.myprocesses.get(query).await;
         assert_eq!(true, resp.is_ok());
     }
 
