a75f58cd7abfd4711b545355e2f25c0052aacbaa
codemountains <4kz12zz@gmail.com>
2022-12-03 23:28:22 +0900

    :sparkles: deleteDocument

--- src/client/document/document_one.rs
+++ src/client/document/document_one.rs
@@ -1,8 +1,6 @@
 use crate::authorization::HEADER_KEY;
 use crate::request::document::document_one::PutDocumentStatusRequest;
-use crate::response::document::document_one::{
-    GetDocumentResponse, GetDocumentResponseBody, PutDocumentStatusResponse,
-};
+use crate::response::document::document_one::{DeleteDocumentResponse, GetDocumentResponse, GetDocumentResponseBody, PutDocumentStatusResponse};
 use crate::response::error::{ErrorResponse, ErrorResponseBody};
 use crate::Query;
 use serde::Serialize;
@@ -104,6 +102,55 @@ impl Document {
 
                 if status == 200 {
                     Ok(PutDocumentStatusResponse { status })
+                } else {
+                    match resp.json::<ErrorResponseBody>().await {
+                        Ok(body) => {
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                        Err(err) => {
+                            let body = ErrorResponseBody {
+                                error: true,
+                                messages: vec![err.to_string()],
+                            };
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                    }
+                }
+            }
+            Err(err) => {
+                let body = ErrorResponseBody {
+                    error: true,
+                    messages: vec![err.to_string()],
+                };
+                let error_response = ErrorResponse { status: 500, body };
+                Err(error_response)
+            }
+        }
+    }
+
+    pub async fn delete(
+        &self,
+        document_id: i32,
+        query: Query,
+    ) -> Result<DeleteDocumentResponse, ErrorResponse> {
+        let request_url = format!("{}/{}", &self.url, document_id);
+
+        let http_client = reqwest::Client::new();
+        let result = http_client
+            .delete(request_url)
+            .query(&query.to_queries())
+            .header(HEADER_KEY, &self.authorization_header)
+            .send()
+            .await;
+
+        match result {
+            Ok(resp) => {
+                let status = resp.status().as_u16();
+
+                if status == 200 {
+                    Ok(DeleteDocumentResponse { status })
                 } else {
                     match resp.json::<ErrorResponseBody>().await {
                         Ok(body) => {
