4060e66e38c6ec7f63039e256a672b2f1b52f33d
codemountains <4kz12zz@gmail.com>
2022-12-04 16:32:29 +0900

    :sparkles: deleteTitle

--- src/client/title/title_one.rs
+++ src/client/title/title_one.rs
@@ -2,9 +2,10 @@ use crate::authorization::HEADER_KEY;
 use crate::record::title::TitleRecord;
 use crate::request::title::title_one::PutTitleRequest;
 use crate::response::error::{ErrorResponse, ErrorResponseBody};
-use crate::response::title::title_one::{GetTitleResponse, PutTitleResponse};
+use crate::response::title::title_one::{DeleteTitleResponse, GetTitleResponse, PutTitleResponse};
 use crate::Query;
 use serde::Serialize;
+use serde_json::Value;
 
 const RESOURCE: &str = "titles";
 
@@ -104,6 +105,65 @@ impl Title {
                 if status == 200 {
                     match resp.json::<TitleRecord>().await {
                         Ok(body) => Ok(PutTitleResponse { status, body }),
+                        Err(err) => {
+                            let body = ErrorResponseBody {
+                                error: true,
+                                messages: vec![err.to_string()],
+                            };
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                    }
+                } else {
+                    match resp.json::<ErrorResponseBody>().await {
+                        Ok(body) => {
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                        Err(err) => {
+                            let body = ErrorResponseBody {
+                                error: true,
+                                messages: vec![err.to_string()],
+                            };
+                            let error_response = ErrorResponse { status, body };
+                            Err(error_response)
+                        }
+                    }
+                }
+            }
+            Err(err) => {
+                let body = ErrorResponseBody {
+                    error: true,
+                    messages: vec![err.to_string()],
+                };
+                let error_response = ErrorResponse { status: 500, body };
+                Err(error_response)
+            }
+        }
+    }
+
+    pub async fn delete(
+        &self,
+        title_id: &str,
+        query: Query,
+    ) -> Result<DeleteTitleResponse, ErrorResponse> {
+        let request_url = format!("{}/{}", &self.url, title_id);
+
+        let http_client = reqwest::Client::new();
+        let result = http_client
+            .delete(request_url)
+            .query(&query.to_queries())
+            .header(HEADER_KEY, &self.authorization_header)
+            .send()
+            .await;
+
+        match result {
+            Ok(resp) => {
+                let status = resp.status().as_u16();
+
+                if status == 200 {
+                    match resp.json::<Value>().await {
+                        Ok(body) => Ok(DeleteTitleResponse { status, body }),
                         Err(err) => {
                             let body = ErrorResponseBody {
                                 error: true,
--- src/response/title/title_one.rs
+++ src/response/title/title_one.rs
@@ -1,5 +1,6 @@
 use crate::record::title::TitleRecord;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Deserialize, Clone, Serialize)]
 pub struct GetTitleResponse {
@@ -11,4 +12,10 @@ pub struct GetTitleResponse {
 pub struct PutTitleResponse {
     pub status: u16,
     pub body: TitleRecord,
+}
+
+#[derive(Debug, Deserialize, Clone, Serialize)]
+pub struct DeleteTitleResponse {
+    pub status: u16,
+    pub body: Value,
 }
